647480a579382788887b4faf8918727634fe37b6
tory99 <hjw990826@gmail.com>
2025-02-05 15:41:21 +0900

    mariadb 연결

---
+++ server.js
@@ -0,0 +1,18 @@
+let http = require('http');
+let url = require('url');
+
+function start(route, handle){
+    function onRequest(request, response) {
+        let pathname = url.parse(request.url).pathname;
+        if( pathname == '/favicon.ico'){
+            response.writeHead(200, { 'Content-Type': 'image/x-icon'});
+            return response.end
+        }
+
+        route(pathname,handle,response);
+    }
+    
+    http.createServer(onRequest).listen(8888);
+}
+
+exports.start = start;
