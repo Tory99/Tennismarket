90ee92c9b2e6e888714572752fdbe32bc4241e54
tory99 <hjw990826@gmail.com>
2025-02-05 15:54:23 +0900

    main 연결

--- requesthandler.js
+++ requesthandler.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const main_view = fs.readFileSync('./main.html', 'UTF-8');
+
 const mariadb = require('./database/connect/mariadb.js');
+
+
 
 function main(response) {
     console.log('main');
@@ -8,23 +13,50 @@ function main(response) {
     })
 
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Main!');
+    response.write(main_view);
     response.end();    
 }
-function login(response) {
-    console.log('login');
 
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Login');
-    response.end();  
+function redRacket(response){
+    fs.readFile('./img/redRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    })
 }
 
+function blueRacket(response){
+    fs.readFile('./img/blueRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
 
+function blackRacket(response){
+    fs.readFile('./img/blackRacket.png', function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+
+function main_css(response){
+    fs.readFile('./main.css', function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(data);
+        response.end();
+    })
+}
 
 
 let handle = {};
 handle['/'] = main;
-handle['/login'] = login;
+handle['/main.css'] = main_css;
+// image directory
+handle['/img/redRacket.png'] = redRacket;
+handle['/img/blueRacket.png'] = blueRacket;
+handle['/img/blackRacket.png'] = blackRacket;
 
 exports.handle = handle;
 
